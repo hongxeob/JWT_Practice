c2256496fb00c7b1c576ad8582fad8ffac18f0f9
Hongseob Lee <sjun2918@naver.com>
2023-08-15 14:51:09 +0900

    :tada: 프로젝트 세팅

---
+++ src/main/java/practice/jwt/JwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package practice.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtPracticeApplication.class, args);
+    }
+
+}
