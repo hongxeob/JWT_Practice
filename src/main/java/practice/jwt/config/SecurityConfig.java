9080d29b4c5714623314f65301628d1eb61e69a2
Hongseob Lee <sjun2918@naver.com>
2023-08-15 15:30:43 +0900

    :sparkles: CorsConfig 클래스 생성 및 Security 필터에 추가

--- src/main/java/practice/jwt/config/SecurityConfig.java
+++ src/main/java/practice/jwt/config/SecurityConfig.java
@@ -1,15 +1,22 @@
 package practice.jwt.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CorsConfig corsConfig;
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
@@ -17,6 +24,8 @@ public class SecurityConfig {
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .apply(new MyCustomDsl())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/apu/v1/users/**")
                 .access("hasRole('USER') or hasRole('ADMIN') or hasRole('MANAGER')")
@@ -26,7 +35,14 @@ public class SecurityConfig {
                 .access("hasRole('ADMIN')")
                 .anyRequest().permitAll()
                 .and().build();
-
-
     }
+
+    public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.addFilter(corsConfig.corsFilter());
+        }
+    }
+
 }
