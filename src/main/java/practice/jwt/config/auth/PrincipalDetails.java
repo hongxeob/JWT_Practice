81c290e3487d5dbae05988b21e778b7cb4d96f9d
Hongseob Lee <sjun2918@naver.com>
2023-08-15 16:58:34 +0900

    :sparkles: Token 생성을 위한 UserDetails, Service 생성

---
+++ src/main/java/practice/jwt/config/auth/PrincipalDetails.java
@@ -0,0 +1,57 @@
+package practice.jwt.config.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import practice.jwt.model.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        user.getRoleList().forEach(s -> {
+            authorities.add(() -> s);
+        });
+
+        return authorities;
+    }
+}
---
+++ src/main/java/practice/jwt/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package practice.jwt.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import practice.jwt.model.User;
+import practice.jwt.model.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+        return new PrincipalDetails(user);
+    }
+
+}
